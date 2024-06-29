6de36e8c63fcd73b2b329959eaa04bca605ff801
kuhle <kuhlemxotwa00@gmail.com>
2024-06-29 17:26:55 +0200

    Implement collect method

--- src/main/java/org/example/interfacesImpl/NumberRangeSummarizerImpl.java
+++ src/main/java/org/example/interfacesImpl/NumberRangeSummarizerImpl.java
@@ -5,6 +5,7 @@ import org.example.interfaces.NumberRangeSummarizer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
 
@@ -22,11 +23,54 @@ public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
         for (int i=0; i< tempArray.length; i++){
             inputList.add(Integer.parseInt(tempArray[i]));
         }
+        inputList.sort(Integer::compareTo);
         return inputList;
     }
 
+
     @Override
     public String summarizedCollection(Collection<Integer> input) {
+
+        if(input == null || input.isEmpty()){
+            return "";
+        }
+
+        else {
+            List<Integer> inputList = new ArrayList<>(input);
+            StringBuilder summarizedString = new StringBuilder();
+
+            //for tracking minimum of the rage
+            int beginning  = inputList.get(0);
+            //for tracking maximum of the range
+            int end = inputList.get(0);
+
+            for(int i=1; i<inputList.size(); i++){
+                //candidate maximum tracking
+                int currentNumber = inputList.get(i);
+                if(currentNumber - 1 == end){
+                    end = currentNumber;
+                }else{
+                    rangeMaker(summarizedString, beginning,end);
+                    beginning = currentNumber;
+                    end = currentNumber;
+                }
+            }
+        }
         return "";
+    }
+
+    private void rangeMaker(StringBuilder summarizedString, int beginning, int end) {
+
+        if(summarizedString.length()>0){
+            summarizedString.append(", ");
+        }
+
+        if(beginning == end){
+            summarizedString.append(beginning);
+        }
+
+        else{
+            summarizedString.append(beginning).append("-").append(end);
+        }
     }
 }
