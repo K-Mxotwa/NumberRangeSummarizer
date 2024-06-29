a74c1e779f5443afcd78ecd94ade58699a02f9df
kuhle <kuhlemxotwa00@gmail.com>
2024-06-29 16:34:49 +0200

    Implement collect method

--- src/main/java/org/example/interfacesImpl/NumberRangeSummarizerImpl.java
+++ src/main/java/org/example/interfacesImpl/NumberRangeSummarizerImpl.java
@@ -2,13 +2,27 @@ package org.example.interfacesImpl;
 
 import org.example.interfaces.NumberRangeSummarizer;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class NumberRangeSummarizerImpl implements NumberRangeSummarizer {
+
+    //Sample input "1,3 6, 7, 8, 12, 13, 14, 15, 21, 22, 23, 24, 31"
+    //Sample output "1, 3, 6-8, 12-15, 21-24, 31"
+
     @Override
     public Collection<Integer> collect(String input) {
-        return List.of();
+
+        //Convert the string into array of strings
+        String[] tempArray = input.split(",");
+        //initialising the list that will hold the integers
+        List<Integer> inputList= new ArrayList<>();
+
+        for (int i=0; i< tempArray.length; i++){
+            inputList.add(Integer.parseInt(tempArray[i]));
+        }
+        return inputList;
     }
 
     @Override
